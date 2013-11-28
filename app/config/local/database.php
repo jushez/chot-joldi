1c77be98de426ec94d251d3ff6d56fea416c9a5c
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2013-11-29 01:46:59 +0600

    Local configuration file added into git ignore

---
+++ app/config/local/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'chot-joldi',
+			'username'  => 'root',
+			'password'  => 'c0mm0n',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		)
+	)
+
+);
