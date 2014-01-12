26421df74f284787add1438bdbe876e904a8ff30
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2014-01-12 19:11:27 +0600

    Bug fix

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('UsersTableSeeder');
 		$this->call('ProfilesTableSeeder');
 		$this->call('VerificationsTableSeeder');
+		$this->call('JobsTableSeeder');
 	}
 
 }
